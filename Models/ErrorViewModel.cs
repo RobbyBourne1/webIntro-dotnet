b5242231251b1e2dc75ea689f11695f912aa14c5
Robby Bourne <robbybourne1@gmail.com>
2017-08-23 10:29:14 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webIntro_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
