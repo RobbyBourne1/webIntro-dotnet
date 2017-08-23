e1fbb24b77a72a16e2ce812cb1471972cb88dc3b
Robby Bourne <robbybourne1@gmail.com>
2017-08-23 11:13:51 -0400

    Api cotroller format

---
+++ Models/ApiModel.cs
@@ -0,0 +1,11 @@
+namespace webIntro_dotnet.Models
+{
+    public class ApiModel
+    {
+        public string Name { get; set; }
+        public int HeightInch { get; set; }
+        public string Motto { get; set; }
+    }
+
+    
+}
